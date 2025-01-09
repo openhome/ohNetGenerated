529862333539539dad921a062ab9a90de8caa111
Bryan T <blondejamtart@gmail.com>
2025-01-09 15:58:11 +0000

    updated ohnet and compiler

--- jenkins_build.py
+++ jenkins_build.py
@@ -110,7 +110,7 @@ class JenkinsBuild():
         if os_platform == 'linux' and arch == 'armel':
             os.environ['CROSS_COMPILE'] = '/usr/local/arm-2011.09/bin/arm-none-linux-gnueabi-'
         if os_platform == 'linux' and arch == 'armhf':
-            os.environ['CROSS_COMPILE'] = '/opt/gcc-linaro-5.3.1-2016.05-i686_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-'
+            os.environ['CROSS_COMPILE'] = '/opt/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-'
         if os_platform == 'linux' and arch == 'rpi':
             os.environ['CROSS_COMPILE'] = '/opt/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-'
         if os_platform == 'Core' and arch == 'ppc32':
